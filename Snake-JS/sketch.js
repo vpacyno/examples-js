303ade4576804fbe445390b9762f06660a1a1ba9
valdas <valdas.pacyno@gmail.com>
2018-08-15 21:35:15 +0300

    Initial commit

---
+++ Snake-JS/sketch.js
@@ -0,0 +1,47 @@
+let snake;
+let food;
+let cols;
+let rows;
+let SCL = 20;
+
+function setup() {
+	createCanvas(600, 600);
+	frameRate(10);
+	cols = floor(width/SCL);
+	rows = floor(height/SCL);
+	snake = new Snake();
+	pickFoodLocation();
+}
+
+function draw() {
+	background(51);
+	snake.update();
+	stroke(51);
+    fill(255, 0, 0);
+	rect(food.x, food.y, SCL, SCL);
+	
+	if (snake.eatsFood(food)) {
+		pickFoodLocation();
+	}
+}
+
+function keyPressed() {
+	if(keyCode === UP_ARROW) {
+		snake.setMoveDirection(0, -1);
+	} else if (keyCode === DOWN_ARROW) {
+		snake.setMoveDirection(0, 1);
+	} else if (keyCode === LEFT_ARROW) {
+		snake.setMoveDirection(-1, 0);
+	} else if (keyCode === RIGHT_ARROW) {
+		snake.setMoveDirection(1, 0);
+	}
+}
+
+function pickFoodLocation() {
+	food = createVector(
+		floor(random(cols)),
+		floor(random(rows))
+	);
+	food.mult(SCL); // multiply cordinates by scale
+
+}
---
+++ Snake-JS/snake.js
@@ -0,0 +1,81 @@
+class Snake {
+    constructor() {
+        this.position = createVector(20, 20);
+        this.xSpeed = 0;
+        this.ySpeed = 0;
+        this.size = SCL;  //square size
+        this.total = 0;  //amount of eated food 
+        this.tail = [];  //tail array
+
+    }
+
+    setMoveDirection(xDir, yDir) {
+        this.xSpeed = xDir * SCL;
+        this.ySpeed = yDir * SCL;
+    }
+
+    update() {
+        if (this.total === this.tail.length) {
+            for (var i=0; i < this.tail.length - 1; i++) {
+
+                this.tail[i] = this.tail[i+1];  // tail (position history) moves to last array element
+            }
+        }
+
+        if (this.total > 0) {
+            this.tail[this.total - 1] = this.position.copy();  //copy head position to tail array
+        }
+
+        const tempX = this.position.x + this.xSpeed;
+        const tempY = this.position.y + this.ySpeed;
+
+        this.position.x = constrain(tempX, 0, width-this.size);
+        this.position.y = constrain(tempY, 0, height-this.size);
+        this.show();
+
+        this.loosesTail();
+    }
+
+    show() {
+        stroke(51);
+        fill(255);
+
+        //show snakes tail
+        for (var i=0; i < this.tail.length; i++) {
+
+            rect(this.tail[i].x, this.tail[i].y, this.size, this.size);
+            
+        }
+
+        //show snakes head
+        rect(this.position.x, this.position.y, this.size, this.size);
+    }
+
+    eatsFood(foodVector) {
+        const d = this.position.dist(foodVector);
+
+        //check if snake and food position almost the same
+        if(d < 1) {
+            this.total++;
+            return true;
+        }
+        
+        return false;
+    }
+
+    loosesTail () {
+        for (var i = 0; i < this.tail.length; i++) {
+            const tailPosition = this.tail[i];
+            const d = this.position.dist(tailPosition);
+
+            if(d < 1) {
+
+                this.total = 0;
+                this.tail = [];  //create empty array
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
